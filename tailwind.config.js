ae0f9c76fa674d08197405194d36cde60a6ddf10
https://github.com/msaad2023/project1 <alschichan@gmail.com>
2023-04-09 08:48:26 -0700

    Add all my files

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+      body : ['Overpass', 'sans-serif'] ,
+      hero : ['Langar', 'sans-serif'] 
+    },
+    colors: {
+      primary: '#303669'
+    },
+    spacing: {
+      112: '28rem',
+      128: '32rem',
+    }
+    },
+  
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
